d4752170d39803b58ac7ec06df2e189107d47c24
Yash Shah <yshah@argusoft.com>
2023-01-19 21:32:02 +0530

    version 1.0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <hr>
+    <div class="form">
+      <input type="text" placeholder="Name">
+      <input type="text" placeholder="Email">
+      <input type="text" placeholder="Password">
+      <input type="text" placeholder="Confirm Password">
+      <button>Register</button>
+    </div>
+  </div>
+  
+</template>
+
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
